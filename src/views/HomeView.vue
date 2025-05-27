46f74d936cd51b0c58d1b8b65ccab459a67f648a
Jose Angel <joseangelsnchz8@gmail.com>
2025-05-27 16:47:18 +0200

    boton borrar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script>
-import { collection, addDoc, getDocs, updateDoc, doc } from "firebase/firestore";
+import { collection, addDoc, getDocs, updateDoc, doc, deleteDoc } from "firebase/firestore";
 import { db } from "@/firebase/config";
 
 export default {
@@ -15,6 +15,9 @@ export default {
   methods: {
 
     // ++++++ Añadir datos en firestore
+    // Este método añade una nueva ciudad a la colección "viajes" en Firestore
+    // Si se está editando una ciudad, actualiza los datos en lugar de añadir una nueva
+    
     async addCity() {
 
       if (this.editingCity) {
@@ -37,6 +40,8 @@ export default {
       this.getCities();
 
     },
+    // ++++++ Obtener datos de firestore
+    // Este método obtiene los datos de la colección "viajes" y los almacena en el array "cities"
     async getCities() {
       this.cities = [];
       const viajes = collection(db, "viajes")
@@ -50,17 +55,30 @@ export default {
         })
       });
     },
+    // ++++++ Editar datos en firestore
+    // Este método permite editar una ciudad seleccionada, cargando sus datos en los campos de entrada
+    // y cambiando el botón de añadir a actualizar
     editCity(city) {
       this.editingCity = city
       this.inputCity = city.title
       this.inputCountry = city.country
     },
+    // ++++++ Actualizar datos en firestore
+    // Este método actualiza los datos de la ciudad que se está editando
+    // utilizando el ID de la ciudad para identificar el documento en Firestore
     async updateCity() {
       const citiRef = doc(db, "viajes", this.editingCity.id);
       await updateDoc(citiRef, {
         title: this.inputCity,
         country: this.inputCountry,
       });
+    },
+    // ++++++ Borrar datos en firestore
+    // Este método borra una ciudad de la colección "viajes" utilizando su ID
+    async borrarCity(id) {
+      const citiRef = doc(db, "viajes", id);
+      await deleteDoc(citiRef);
+      this.getCities();
     }
   },
   // Cuando el componente o vista está lista para el usuario, es visible, se ejecuta el método mounted
@@ -81,7 +99,9 @@ export default {
 
     <ul style="margin-top: 50px;">
       <li v-for="city in cities" :key="city.id">{{ city.title }} - {{ city.country }} <button
-          @click="editCity(city)">Editar</button></li>
+          @click="editCity(city)">Editar</button>
+          <button @click="borrarCity(city.id)">Borrar</button>
+          </li>
     </ul>
 
   </main>
